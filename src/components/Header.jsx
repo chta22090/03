7a91df45fc723a587cd044979dd122cdfdd5b6cc
chta22090 <chta22090@g.nihon-u.ac.jp>
2024-10-13 21:52:15 +0900

    演習の修正2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
       <header>
         <h1>Birdwatching</h1>
-        <img src="/src/images/dove.png" alt="a simple dove logo" />
+        <img src="/images/dove.png" alt="a simple dove logo" />
       </header>
     );
   }
